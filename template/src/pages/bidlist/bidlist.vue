b0d5f32735f3b42939256e6015d272c94e319d5b
windknow <songhlc@yonyou.com>
2016-12-05 09:22:16 +0800

    添加elementui以及 i18n配置

--- template/src/pages/bidlist/bidlist.vue
+++ template/src/pages/bidlist/bidlist.vue
@@ -1,13 +1,33 @@
 <template>
-  <div>
-    {{data}}
+  <div class="m">
+    <el-breadcrumb separator="/">
+      <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>招标立项</el-breadcrumb-item>
+    </el-breadcrumb>
+    <el-row class="m-tb">
+      <el-col :span="24">
+        <el-tabs :active-name="activeName">
+          <el-tab-pane label="用户管理" name="first"></el-tab-pane>
+          <el-tab-pane label="配置管理" name="second"></el-tab-pane>
+          <el-tab-pane label="角色管理" name="third"></el-tab-pane>
+          <el-tab-pane label="定时任务补偿" name="fourth"></el-tab-pane>
+        </el-tabs>
+      </el-col>
+    </el-row>
+
   </div>
 </template>
 <script>
   export default {
     data () {
       return {
-        data: 4
+        data: 34,
+        activeName: 'first'
+      }
+    },
+    watch: {
+      activeName: function (val, oldvalue) {
+        console.log(val)
       }
     }
   }
